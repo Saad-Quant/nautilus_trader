504ba74674464432f9e886891e64e595959250c1
Saad-Quant <s.patel@soar.capital>
2024-10-30 20:26:33 +0530

    adding DBN to CSV functionality

--- prototype/data_preprocessing.py
+++ prototype/data_preprocessing.py
@@ -1,3 +1,26 @@
+#Databento to csv conversion
+import databento as db
+import pandas as pd
+
+dbn_file_path = './glbx-mdp3-20190821-20240820.ohlcv-1m_v2.dbn'
+# output_csv_path = './nq_futures_data.csv'
+
+# Open the DBN file
+dbn = db.DBNStore.from_file(dbn_file_path)
+
+metadata = dbn.metadata
+print(f"Dataset: {metadata.dataset}")
+print(f"Schema: {metadata.schema}")
+print(f"Start: {metadata.start}")
+print(f"End: {metadata.end}")
+
+# Read the data into a pandas DataFrame
+df = dbn.to_df()
+#df.to_csv("./Nasdaq_1min_check.csv")
+df
+#=================================
+
+#Csv data prerpocessing
 
 import pandas as pd
 data = pd.read_csv("Nasdaq_1min_dbn.csv")
